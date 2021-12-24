146b39fcc0dcb87c026eddc14ac37472a48559a7
Harry Candlish <hcnd90@gmail.com>
2021-12-25 10:21:03 +1300

    home page and added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'index/index'
-  get 'home/index'
+  get 'home/about'
+  root 'home#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
